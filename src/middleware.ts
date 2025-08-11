5721613f2af0e2421ec20e36fdf1b1ee26025c1a
Sandeep Mareeswaran <sandeepmaree2005@gmail.com>
2025-08-11 08:10:14 +0000

    TypeError: auth(...).protect is not a function     at <unknown> (file://

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,13 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher([
-  '/chat(.*)',
-  '/profile(.*)',
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/sign-in(.*)',
+  '/sign-up(.*)',
 ]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
     auth().protect();
   }
 });
