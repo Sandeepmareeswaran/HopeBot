b804869d9d48090e872fdff90529dc04cd548db1
Sandeep Mareeswaran <sandeepmaree2005@gmail.com>
2025-08-11 09:52:38 +0000

    same error that comes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,10 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)']);
 
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect();
-  }
+// The clerkMiddleware function automatically protects all routes that are not public.
+// There is no need for a callback with auth().protect().
+export default clerkMiddleware({
+  publicRoutes: ['/', '/sign-in', '/sign-up'],
 });
 
 export const config = {
