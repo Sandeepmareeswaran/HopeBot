3bac12dfd25daa3827854ceec3c4f4c7e79f0341
Sandeep Mareeswaran <sandeepmaree2005@gmail.com>
2025-08-11 09:47:48 +0000

    after signed in it again return me to the sign in page

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex justify-center items-center h-full">
-      <SignUp />
+      <SignUp afterSignUpUrl="/chat" />
     </div>
   )
 }
